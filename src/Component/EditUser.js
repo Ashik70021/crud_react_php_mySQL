4f07902de009f8b21ee11e098179dff9d4059a37
Ashik70021 <ashik70021@gmail.com>
2025-06-02 15:13:08 +0600

    implemet put method and update data

--- src/Component/EditUser.js
+++ src/Component/EditUser.js
@@ -28,7 +28,7 @@ export default function EditUser() {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        axios.post('http://localhost:80/api/user/save', inputs).then(function (response) {
+        axios.put(`http://localhost:80/api/user/${id}/edit`, inputs).then(function (response) {
             console.log(response.data);
             navigate('/');
         });
