e7a2955999fbaccff8201c04eb209a00cd1df687
Ashik70021 <ashik70021@gmail.com>
2025-06-02 14:52:07 +0600

    Implement edit pages and fetch specific user data

--- src/Component/EditUser.js
+++ src/Component/EditUser.js
@@ -1,5 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function EditUser() {
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState({})
+    const {id} = useParams();
+
+    useEffect(() => {
+        getUser();
+    }, []);
+    function getUser() {
+        axios.get(`http://localhost:80/api/user/${id}`).then(function (response) {
+            console.log(response.data);
+            setInputs(response.data[0])
+        });
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:80/api/user/save', inputs).then(function (response) {
+            console.log(response.data);
+            navigate('/');
+        });
+
+    }
     return (
-        <h1>Edit User</h1>
+        <div align="center">
+            <h1>Edit User</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing={10}>
+                    <tbody>
+                        <tr>
+                            <th><label>Name:</label></th>
+                            <td><input value={inputs.name} type="text" name="name" onChange={handleChange}></input></td>
+                        </tr>
+                        <tr>
+                            <th><label>Email:</label></th>
+                            <td><input value={inputs.email} type="text" name="email" onChange={handleChange}></input></td>
+                        </tr>
+                        <tr>
+                            <th><label>Mobile:</label></th>
+                            <td><input value={inputs.mobile} type="number" name="mobile" onChange={handleChange}></input></td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2} align="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </form>
+        </div>
     )
 }
